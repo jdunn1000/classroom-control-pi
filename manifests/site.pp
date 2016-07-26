ca8d7ae8dc910859984c3871bcb6f7e070dd777b
jdunn1000 <jdunn1000@me.com>
2016-07-26 14:53:07 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
 notify { "Hello world! I am ${::fqdn}": }
 }
-node 'training.puppetlabs.vm' {
+node 'jdunn1000.puppetlabs.vm' {
 # comment this out when done with the exercise
 notify { "This will only be enforced on the Linux container.": }
 }
